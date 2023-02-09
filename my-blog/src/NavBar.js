36f54fce369633f67055f03b5a283c5e28f3a63c
RayMaldonadoEs <ray.maldonado@kw.com>
2023-02-09 14:40:47 -0600

    Chapter 5 Final - Firebase Auth

--- my-blog/src/NavBar.js
+++ my-blog/src/NavBar.js
@@ -1,19 +1,33 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from 'react-router-dom';
+import { getAuth, signOut } from 'firebase/auth';
+import useUser from './hooks/useUser';
 
 const NavBar = () => {
+    const { user } = useUser();
+    const navigate = useNavigate();
+
     return (
         <nav>
             <ul>
-                <Link to="/">
-                    <li>Home</li>
-                </Link>
-                <Link to="/about">
-                    <li>About</li>
-                </Link>
-                <Link to="/articles">
-                    <li>Articles</li>
-                </Link>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+                <li>
+                    <Link to="/articles">Articles</Link>
+                </li>
             </ul>
+            <div className="nav-right">
+                {user
+                    ? <button onClick={() => {
+                        signOut(getAuth());
+                    }}>Log Out</button>
+                    : <button onClick={() => {
+                        navigate('/login');
+                    }}>Log In</button>}
+            </div>
         </nav>
     );
 }
--- my-blog/src/index.js
+++ my-blog/src/index.js
@@ -4,6 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB5pHeM9p11bIEazAeIXN5RrgRt2VuPWxM",
+  authDomain: "my-react-blog-676db.firebaseapp.com",
+  projectId: "my-react-blog-676db",
+  storageBucket: "my-react-blog-676db.appspot.com",
+  messagingSenderId: "710433388424",
+  appId: "1:710433388424:web:65b909eef6e17d043dec3d"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
